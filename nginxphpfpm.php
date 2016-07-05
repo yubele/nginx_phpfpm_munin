8bd4da5c5435d0bfe4696b602558ea2fcca9b57b
yusuke abe <yusuke@newsdict.jp>
2016-07-05 12:07:56 +0000

    Revert "update nginxphpfpm.info"
    This reverts commit 968b92bffb032fc560be5ee38c8880e2aa710fc7.
    
    Conflicts:
    	nginxphpfpm.php

--- nginxphpfpm.php
+++ nginxphpfpm.php
@@ -1,6 +1,14 @@
 #!/usr/bin/php
 <?php
 
+echo('graph_title nginx phpfpm status'."\n");
+echo('graph_order down up'."\n");
+echo('graph_vlabel conn/processes'."\n");
+echo('graph_scale no'."\n");
+echo('graph_info NGINX & php-fpm status'."\n");
+echo('graph_category nginx'."\n");
+echo('graph_period minute'."\n");
+
 $phpfpm[0] = "accepted conn";
 $phpfpm[1] = "max listen queue";
 $phpfpm[2] = "listen queue len";
@@ -21,14 +29,10 @@ $i=0;
 foreach($phpfpm as $label) {
   $status = chop(`wget 127.0.0.1/phpfpm_status -O - -q | egrep -e "^$label:" | cut -f2 -d: |awk '{print $1}'`);
   $replacement_label = preg_replace("#\s#", '_', $label);
-  echo('graph_title '.$label."\n");
-  echo('graph_order down up'."\n");
-  echo('graph_vlabel conn/processes'."\n");
-  echo('graph_scale no'."\n");
-  echo('graph_category nginx'."\n");
-  echo('graph_period minute'."\n");
   echo("$replacement_label.label ".$replacement_label."\n");
   echo("$replacement_label.value ".$status."\n");
+  echo($replacement_label.'.type DERIVE'."\n");
+  echo($replacement_label.'.info '.$label."\n");
 }
 
 #wget 127.0.0.1/phpfpm_status -O - -q | grep "max children reached" | cut -f2 -d: |awk '{print $1}'
